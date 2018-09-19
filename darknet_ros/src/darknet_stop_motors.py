66f6bc20dfdc6d6ff4b277fa5933f037a467a4f8
Alberto <alberto.dionigi95@gmail.com>
2018-09-19 13:25:22 +0200

    Aggiornato il nodo per spegnere i motori quando si vede una papera

--- darknet_ros/src/darknet_stop_motors.py
+++ darknet_ros/src/darknet_stop_motors.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
 import rospy,sys,roslib
-from geometry_msgs.msg import Twist,Vector3
 from master_node.srv import *
 from std_msgs.msg import Int8
 
-
-twistmessage = Twist()
-twistmessage.linear.x = 0
-twistmessage.angular.z = 0
-
-requested_service = False
 
 stop_service = rospy.ServiceProxy('stop',StopService)
 
@@ -21,24 +14,24 @@ def mainFunction():
     rospy.init_node('darknet_stop_motors', anonymous=True) # init a darknet_stop_motors node
 
     #Sottosrizione al topic object_detector
-    rospy.Subscriber("/darknet_ros/object_detector",Int8,callback)
+    rospy.Subscriber("/darknet_ros/found_object",Int8,callback)
 
     rospy.loginfo("Stop Service Ready with DarknetROS")
 
     #REALEASE SULLO SHUTDOWN
-    rospy.on_shutdown(print("Stop Service DarknetROS shutted down"))
+    #rospy.on_shutdown(print("Stop Service DarknetROS shutted down"))
     rospy.spin()
 
 
 
 def callback(data):
-	duck_found = data.data
+	ducks_founded = data.data
+	print(ducks_founded)
 
-	if((not requested_service) and duck_found != 0):
-		stop_service(twistmessage)
-		requested_service = True
-	else:
-		requested_service = False
+
+	if (ducks_founded > 0):
+		print("Founded")
+		stop_service(int(ducks_founded))
 
 
 
